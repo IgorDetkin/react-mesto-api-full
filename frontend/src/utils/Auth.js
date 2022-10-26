fdfa59bca7b48cd42da84ba7db443e4ded05891f
Igor Detkin <dean94@bk.ru>
2022-10-26 17:53:07 +0600

    15sprint_7

--- frontend/src/utils/Auth.js
+++ frontend/src/utils/Auth.js
@@ -31,8 +31,7 @@ export const authorize = ({email, password}) => {
       method: 'POST',
       headers: {
         'Accept': 'application/json',
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${token}`
+        'Content-Type': 'application/json'
       },
       body: JSON.stringify({
         email, password
