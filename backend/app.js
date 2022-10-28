8ec7170400b8676c874455e7d14fe46aee31695e
Igor Detkin <dean94@bk.ru>
2022-10-28 19:51:24 +0600

    15sprint_25

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
-const path = require('path');
+// const path = require('path');
 // require('dotenv').config({ path: path.join(__dirname, '.env') }); // ??????
-require('dotenv').config({ path: path.join(__dirname, '.env') });
-require('dotenv').config({ path: '/home/devopsina/.env' });
+// require('dotenv').config({ path: path.join(__dirname, '.env') });
+// require('dotenv').config({ path: '/home/devopsina/.env' });
 // require('dotenv').config({ path: './middlewares/.env' });
 require('dotenv').config();
 const express = require('express');
